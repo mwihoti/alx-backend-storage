c3266aeb80d22f20e12a0909231edf35c5eabb05
Danmwihoti <danielmwihoti@gmail.com>
2024-08-08 23:05:26 +0300

    No table for a meeting

--- 0x00-MySQL_Advanced/11-safe_div.sql
+++ 0x00-MySQL_Advanced/11-safe_div.sql
@@ -3,5 +3,4 @@
 CREATE VIEW need_meeting AS
     SELECT name
         FROM students
-        WHERE score < 80 AND ( last_meeting IS NULL OR last_meeting < SUBDATE(CURRDATE(), INTERVAL 1 MONTH)
-        );
+        WHERE score < 80 AND ( last_meeting IS NULL OR last_meeting < SUBDATE(CURRENT_DATE(), INTERVAL 1 MONTH));
