0a91d40e36194548be593f18998e6d8c0a059a6b
Danmwihoti <danielmwihoti@gmail.com>
2024-08-09 21:42:32 +0300

    average weight score

--- 0x00-MySQL_Advanced/100-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -1,34 +1,34 @@
 -- SQL script that creates a stored procedure ComputeAverageWeightedScoreForUser that computes and store the average weighted score for a student.
 
-DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
+DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUser;
 DELIMITER $$
-CREATE PROCEDURE ComputeAverageScoreForUser(user_id INT)
+CREATE PROCEDURE ComputeAverageWeightedScoreForUser (user_id INT)
 BEGIN
-    DECLARE total_weight_score FLOAT DEFAULT 0;
-    DECLARE total_weight FLOAT DEFAULT 0;
+    DECLARE total_weighted_score INT DEFAULT 0;
+    DECLARE total_weight INT DEFAULT 0;
 
     SELECT SUM(corrections.score * projects.weight)
-        INTO total_weight_score
+        INTO total_weighted_score
         FROM corrections
             INNER JOIN projects
                 ON corrections.project_id = projects.id
         WHERE corrections.user_id = user_id;
-    
+
     SELECT SUM(projects.weight)
         INTO total_weight
         FROM corrections
             INNER JOIN projects
                 ON corrections.project_id = projects.id
         WHERE corrections.user_id = user_id;
-    
+
     IF total_weight = 0 THEN
         UPDATE users
-            SET average_score = 0
-            WHERE id = user_id;
+            SET users.average_score = 0
+            WHERE users.id = user_id;
     ELSE
         UPDATE users
-        SET users.average_score = total_weight_score / total_weight
-        WHERE id = user_id;
+            SET users.average_score = total_weighted_score / total_weight
+            WHERE users.id = user_id;
     END IF;
 END $$
 DELIMITER ;
