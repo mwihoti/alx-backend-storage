74d40f33c440ba2b79065dd0f96c5464e6243038
Danmwihoti <danielmwihoti@gmail.com>
2024-08-09 20:52:01 +0300

    average weightt

--- 0x00-MySQL_Advanced/100-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -4,8 +4,8 @@ DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
 DELIMITER $$
 CREATE PROCEDURE ComputeAverageScoreForUser(user_id INT)
 BEGIN
-    DECLARE total_weight_score INT DEFAULT 0;
-    DECLARE total_weight INT DEFAULT 0;
+    DECLARE total_weight_score FLOAT DEFAULT 0;
+    DECLARE total_weight FLOAT DEFAULT 0;
 
     SELECT SUM(corrections.score * projects.weight)
         INTO total_weight_score
@@ -23,12 +23,12 @@ BEGIN
     
     IF total_weight = 0 THEN
         UPDATE users
-            SET users.average_score = 0
-            WHERE users.id = user_id;
+            SET average_score = 0
+            WHERE id = user_id;
     ELSE
         UPDATE users
         SET users.average_score = total_weight_score / total_weight
-        WHERE users.id = user_id;
+        WHERE id = user_id;
     END IF;
 END $$
 DELIMITER ;
