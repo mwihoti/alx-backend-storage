073c530639fcd0809f4ef458382489f0e64c8d4a
Danmwihoti <danielmwihoti@gmail.com>
2024-08-13 16:07:31 +0300

    Insert a document in Python

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Python function that inserts a new document in a collection based on kwargs
+"""
+
+
+def insert_school(mongo_collection, **kwargs):
+    """
+    
+    Insert a document in a collection based on kwargs
+    """
+    newColl = mongo_collection.insert_one(kwargs)
+    return newColl.inserted_id
